016736387ba24e32b8a1f93d8ea13a3f1eca000a
Your Username <you@example.com>
2021-04-24 23:14:31 +0530

    new deck

--- deck.go
+++ deck.go
@@ -4,6 +4,20 @@ import "fmt"
 
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"spades", "Diamonds", "Hearts", "clubs"}
+	cardValues := []string{"Ace", "two", "three", "four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
