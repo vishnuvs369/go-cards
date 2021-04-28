4da9181af5e40f27a9076343efc420f45a60e1e5
Your Username <you@example.com>
2021-04-28 22:00:33 +0530

    first commit

--- deck.go
+++ deck.go
@@ -1,6 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
 
 type deck []string
 
@@ -21,5 +28,38 @@ func newDeck() deck {
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
+	}
+}
+
+func deal(d deck, handsize int) (deck, deck) {
+	return d[:handsize], d[handsize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error :", err)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs), ",")
+	return deck(s)
+}
+
+func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+
+		d[i], d[newPosition] = d[newPosition], d[i]
 	}
 }
--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
 func main() {
-	cards := newDeck()
+	// cards := newDeck()
 
+	// hand, remainingCards := deal(cards, 5)
+
+	// hand.print()
+	// remainingCards.print()
+
+	//-->converting to byte
+	// greeting := "Hi there"
+	// fmt.Print([]byte(greeting))
+
+	//-->to save to hard disk
+	// cards := newDeck()
+	// cards.saveToFile("my_cards")
+
+	//-->open from harddisk
+	//cards := newDeckFromFile("my_cards")
+	//cards := newDeckFromFile("my")----->to get error
+	//cards.print()
+
+	//shuffle-->
+	cards := newDeck()
+	cards.shuffle()
 	cards.print()
+
 }
